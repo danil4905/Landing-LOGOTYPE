38c6733cea61922cd227f74d4f031505caace938
danil4905 <47856599+danil4905@users.noreply.github.com>
2019-12-25 18:14:59 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(function(){
+
+  $('.slider__inner, .news__slider-inner').slick({
+    nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+    prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+    infinite: false
+  });
+
+  $('select').styler();
+
+  $('.header__btn-menu').on('click', function(){
+    $('.menu ul').slideToggle();
+  });
+
+});
